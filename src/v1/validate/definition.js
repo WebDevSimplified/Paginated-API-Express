b01374c969610e6543d48f491b71d80b04ea5093
Derrick Mbarani <derrickmbarani@gmail.com>
2023-06-18 14:20:48 +0300

    add post endpoint

---
+++ src/v1/validate/definition.js
@@ -0,0 +1,56 @@
+const validate = {};
+
+validate.validateCreate = (doc) => {
+  if (Object.keys(doc).length != 7) return false;
+  let {
+    title,
+    definition,
+    category,
+    exampleUsage,
+    partOfSpeech,
+    rarity,
+    spellingVariations,
+  } = doc;
+  title = typeof title == "string" && title.trim() != "" ? title.trim() : false;
+  definition =
+    typeof definition == "string" && definition.trim() != ""
+      ? definition.trim()
+      : false;
+  category =
+    typeof category == "string" && category.trim() != ""
+      ? category.trim()
+      : false;
+  exampleUsage =
+    typeof exampleUsage == "string" && exampleUsage.trim() != ""
+      ? exampleUsage.trim()
+      : false;
+  partOfSpeech =
+    typeof partOfSpeech == "string" && partOfSpeech.trim() != ""
+      ? partOfSpeech.trim()
+      : false;
+  rarity =
+    typeof rarity == "string" && rarity.trim() != "" ? rarity.trim() : false;
+  spellingVariations =
+    typeof spellingVariations == "string" && spellingVariations.trim() != ""
+      ? spellingVariations.trim()
+      : spellingVariations == null
+      ? null
+      : false;
+
+  if (
+    !(
+      title &&
+      definition &&
+      category &&
+      exampleUsage &&
+      partOfSpeech &&
+      rarity &&
+      spellingVariations != false
+    )
+  ) {
+    return false;
+  }
+  return true;
+};
+
+module.exports = validate;
